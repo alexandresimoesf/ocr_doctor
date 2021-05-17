d70c4c96776fee140f001fefa4b0d648381adddf
alexandresimoesf <alexandresimoesf@gmail.com>
2021-05-17 18:16:58 -0300

    base facade class added

---
+++ base/delete.py
@@ -0,0 +1,4 @@
+class Delete:
+
+    def delete_single_element(self):
+        print('deleta um registro')
---
+++ base/insert.py
@@ -0,0 +1,7 @@
+class Insert:
+
+    def insert_single_element(self):
+        print('insere um unico registro')
+
+    def insert_many_elements(self):
+        print('insere varios registros')
